cce2b49dd34f2c210a8c100e81afd7b0e456b161
Paul Tarazona <paul.tarazona@unas.edu.pe>
2024-11-03 20:59:40 -0500

    Correccion de bug

--- Proyectos/P.02_Calculadora-1.0/JS/Main.js
+++ Proyectos/P.02_Calculadora-1.0/JS/Main.js
@@ -4,29 +4,44 @@ let numberOne = form['numberOne'];
 let numberTwo = form['numberTwo'];
 let respuesta = 0;
 
+function validar(respuesta){
+    if(isNaN(respuesta)){
+        return respuesta = "No se puede realizar la operacion, ingrese numeros"
+    }else{
+        return respuesta;
+    }
+}
+
+
 
 function sumar(){
     respuesta = parseFloat(numberOne.value) + parseFloat(numberTwo.value);
-    document.getElementById('respuesta').innerHTML = `La suma es: ${respuesta}`
+    respuesta = validar(respuesta);
+    document.getElementById('respuesta').innerHTML = `Respuesta: ${respuesta}`
 }
 function restar(){
  respuesta = parseFloat(numberOne.value) - parseFloat(numberTwo.value);
-    document.getElementById('respuesta').innerHTML = `La resta es: ${respuesta}`
+ respuesta = validar(respuesta);
+    document.getElementById('respuesta').innerHTML = `Respuesta: ${respuesta}`
 }
 
 function multiplicar(){
     respuesta = parseFloat(numberOne.value) * parseFloat(numberTwo.value);
-    document.getElementById('respuesta').innerHTML = `La multiplicacion es: ${respuesta}`
+    respuesta = validar(respuesta);
+    document.getElementById('respuesta').innerHTML = `Respuesta: ${respuesta}`
 }
 
 function dividir(){
     let MAX = parseFloat(numberOne.value);
     let MIN = parseFloat(numberTwo.value);
 
+    respuesta = validar(MIN);
+    document.getElementById('respuesta').innerHTML = `Respuesta: ${respuesta}`
 
     if(MIN>0){
         respuesta = MAX/MIN;
-        document.getElementById('respuesta').innerHTML = `La division es: ${respuesta}`
+        respuesta = validar(respuesta);
+        document.getElementById('respuesta').innerHTML = `Respuesta: ${respuesta}`
     }else{
         respuesta = "No se puede dividir";
         document.getElementById('respuesta').innerHTML = `${respuesta}`
